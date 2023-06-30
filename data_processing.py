10bec4730db08d4e925f00037b4e114afb496b8b
Louise_git_user_name <wyongyi07@gmail.com>
2023-06-30 21:25:53 +0100

    Initial commit

---
+++ data_processing.py
@@ -0,0 +1,70 @@
+﻿# 处理csv中的数据
+import pandas as pd
+import ast
+
+pd.set_option('display.max_columns', None)
+
+# # 读取0, 2, 11, 12, 13, 14列
+# df = pd.read_csv('EPIC_100_train.csv')
+# df['all_nouns'] = df['all_nouns'].apply(ast.literal_eval)
+# df['all_noun_classes'] = df['all_noun_classes'].apply(ast.literal_eval)
+# selected_columns = df.iloc[:, [0, 2, 11, 12, 13, 14]]
+# # print(selected_columns.head(10))
+# selected_columns.to_csv('new_EPIC_100_train.csv', index=False)
+
+# # 展开
+# df_exp = selected_columns.explode(['all_noun_classes', 'all_nouns'])
+# df_exp = df_exp.reset_index(drop=True)
+# print(df_exp.to_string())
+
+# #排序
+# value_counts = df_exp['all_nouns'].value_counts().sort_values(ascending=False)
+# # 获取排序前 20 位的值
+# top_20_values = value_counts.head(20).index.tolist()
+# print(top_20_values)
+#
+# # 保留属于排序前 20 的行，删除剩下的行
+# df_filtered = df_exp[df_exp['all_nouns'].isin(top_20_values)]
+# print(df_filtered)
+#
+#
+# # 将结果输出到新的 CSV 文件
+# df_filtered.to_csv('front20.csv', index=False)
+
+
+# P01_109, P02_108, P02_112, P02_129, P04_107中所有位于前二十的帧数总和 1840
+
+
+
+
+# 筛选以指定数字开头的行
+df = pd.read_csv("file/front20.csv")
+# selected_rows = df[df['narration_id'].str.startswith(('P01_109', 'P02_108', 'P02_112', 'P02_129', 'P04_107'))]
+selected_rows = df[df['narration_id'].str.startswith(('P01_109', 'P02_108'))]
+selected_rows['narration_id'] = selected_rows['narration_id'].astype(str) + ".jpg"
+print(selected_rows.to_string())
+print(selected_rows.shape[0])
+
+# 筛选列
+selected_columns = selected_rows.iloc[:, [0, 2, 3]]
+
+
+selected_columns.to_csv('label.csv', index=False)
+
+
+
+
+# # 把csv中的名字前缀全去掉，只留下最后一个代表本视频序号的数字
+# file_path = "P01_109.csv"  # 文件路径
+#
+# # 读取CSV文件
+# df = pd.read_csv(file_path)
+#
+# # 提取数字部分
+# df['narration_id'] = df['narration_id'].str.replace('P01_109_', '').str.replace('.jpg', '')
+#
+# # 保存修改后的数据到CSV文件
+# df.to_csv('P01_109(2).csv', index=False)
+
+
+
---
+++ data_processing2.py
@@ -0,0 +1,70 @@
+﻿# 处理图片数据
+import os
+import pandas as pd
+
+# 更改文件夹中图片的名称，全都改为P01_109_1的格式
+folder_path = "P02_108"  # 文件夹路径
+prefix = "P02_108_"  # 新的文件名前缀
+
+file_list = os.listdir(folder_path)  # 获取文件夹中的文件列表
+
+for i, file_name in enumerate(file_list):
+    _, ext = os.path.splitext(file_name)  # 获取文件名和扩展名
+    new_file_name = f"{prefix}{i+1}{ext}"  # 构建新的文件名
+    old_file_path = os.path.join(folder_path, file_name)  # 原始文件路径
+    new_file_path = os.path.join(folder_path, new_file_name)  # 新文件路径
+    os.rename(old_file_path, new_file_path)  # 重命名文件
+
+ext = '.jpg'
+for i in range(500):
+    # _, ext = os.path.splitext(file_name)  # 获取文件名和扩展名
+    frame_number_string = str(i).zfill(10)
+    orig_file_name = f"frame_{frame_number_string}{ext}"
+    print(orig_file_name)
+    new_file_name = f"{prefix}{i+1}{ext}"  # 构建新的文件名
+    print(new_file_name)
+    old_file_path = os.path.join(folder_path, orig_file_name)  # 原始文件路径
+    new_file_path = os.path.join(folder_path, new_file_name)  # 新文件路径
+    os.rename(old_file_path, new_file_path)  # 重命名文件
+
+
+
+# # 按照csv中的顺序在文件夹中排列图片
+# folder_path = "P02_108"  # 文件夹路径
+# csv_file = "P02_108.csv"  # CSV文件路径
+#
+# # 读取CSV文件
+# df = pd.read_csv(csv_file)
+#
+# # 获取CSV文件第一列的图片名称列表
+# csv_image_names = df['narration_id'].tolist()
+#
+# print(csv_image_names)
+#
+# # 遍历文件夹中的文件
+# for file_name in os.listdir(folder_path):
+#     file_path = os.path.join(folder_path, file_name)
+#     if file_name not in csv_image_names:
+#         # 删除不存在于CSV文件中的图片
+#         os.remove(file_path)
+#         print(f"删除文件: {file_name}")
+
+
+
+
+# # 输出CSV文件中存在但文件夹中不存在的图片的narration_id：
+#
+# folder_path = "P01_109"  # 文件夹路径
+# csv_file = "P01_109.csv"  # CSV文件路径
+#
+# # 读取CSV文件
+# df = pd.read_csv(csv_file)
+#
+# # 获取CSV文件中的narration_id列数据
+# csv_narration_ids = df['narration_id'].tolist()
+#
+# # 遍历CSV文件中的narration_id
+# for narration_id in csv_narration_ids:
+#     file_path = os.path.join(folder_path, f"{narration_id}")
+#     if not os.path.exists(file_path):
+#         print(f"文件夹中不存在的图片的narration_id: {narration_id}")
